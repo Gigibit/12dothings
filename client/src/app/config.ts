ae04944b6a253967c1e27d0fbec3f129119ac6a9
BlueScreen <l.chougrad@gmail.com>
2019-05-22 08:46:27 +0200

    wip on layouts

--- client/src/app/config.ts
+++ client/src/app/config.ts
@@ -1,2 +1,2 @@
-export const AUTH_SERVER = 'http://192.168.1.4:3001'
-export const SERVICE_SERVER = 'http://192.168.1.4:3001'
+export const AUTH_SERVER = 'http://192.168.1.7:3001'
+export const SERVICE_SERVER = 'http://192.168.1.7:3001'
